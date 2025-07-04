33c2e06e963a660c45063c07e391d1c6aa1bc67a
KhacTL <lktin@tuta.io>
2025-07-04 05:16:07 +0000

    Initial ASP.NET Core commit

---
+++ TL-Inc/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TL_Inc.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
